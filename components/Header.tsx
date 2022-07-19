00fee6c6909ebf0867f944c106291400cb9cfc0a
si-shakil <sishakil850@gmail.com>
2022-07-20 00:45:31 +0600

    header is updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,25 @@
 import Image from "next/image";
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 function Header() {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    };
+  }, []);
   return (
-    <header>
+    <header className={`${isScrolling && "bg-[#141414]"}`}>
       {/* Left Side */}
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
@@ -11,7 +29,7 @@ function Header() {
           alt="Netflix Profile"
           className="cursor-pointer object-contain"
         />
-        <ul>
+        <ul className="hidden space-x-4 md:flex">
           <li className="linkOfHeader">Home</li>
           <li className="linkOfHeader">TV Shows</li>
           <li className="linkOfHeader">Movies</li>
@@ -20,7 +38,18 @@ function Header() {
         </ul>
       </div>
       {/* Right Side */}
-      <div></div>
+      <div className="flex items-center space-x-4 text-sm">
+        <SearchIcon className="hidden h-6 w-6 sm:inline cursor-pointer" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6 cursor-pointer" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt="user profile"
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
   );
 }
