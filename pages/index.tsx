cb3f9ded68e8ecf2632d963f226da3b4cf97e91f
si-shakil <sishakil850@gmail.com>
2022-07-23 23:36:41 +0600

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-26">
         <Banner netflixOriginals={netflixOriginals} />
       </main>
     </div>
